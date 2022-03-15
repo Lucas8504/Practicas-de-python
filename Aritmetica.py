0d47b54447e3a37e247904599da51ee3287ff89c
Lucas Araujo <lsa1@live.com.ar>
2022-03-14 22:41:56 -0300

    ejercicio aritmetica 2

--- Aritmetica.py
+++ Aritmetica.py
@@ -2,6 +2,7 @@ class Aritmetica:
     """
     Clase aritmetica para sumar, restar, ect
     """
+
     def __init__(self, operandoA, operandoB):
         self.operandoA = operandoA
         self.operandoB = operandoB
@@ -9,5 +10,18 @@ class Aritmetica:
     def Sumar(self):
         return self.operandoA + self.operandoB
 
-aritmetica1 = Aritmetica(5,3)
-print(aritmetica1.Sumar())
+    def Restar(self):
+        return self.operandoA - self.operandoB
+
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+
+    def dividir(self):
+        return self.operandoA / self.operandoB
+
+
+aritmetica1 = Aritmetica(5, 3)
+print(f"suma = {aritmetica1.Sumar()}")
+print(f"resta = {aritmetica1.Restar()}")
+print(f"Multiplicacion = {aritmetica1.multiplicar()}")
+print(f"Divicion = {aritmetica1.dividir()}")
