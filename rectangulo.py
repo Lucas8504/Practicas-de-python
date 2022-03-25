2a4b057cfb9ab88d8d4b01a974567cf5ec880d3e
Lucas Araujo <lsa1@live.com.ar>
2022-03-25 14:15:29 -0300

    ejercicio lab rectangulo

---
+++ rectangulo.py
@@ -0,0 +1,11 @@
+class Rectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return f"Area del rectangulo: {self.base * self.altura}"
+
+
+p = Rectangulo(int(input("Proporciona la base: ")), int(input("Proporciona la altura: ")))
+print(p.area())
