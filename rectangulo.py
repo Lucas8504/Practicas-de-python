cf6334e4acfbd1bc81466dcc4d1978043fdb5ca6
Lucas Araujo <lsa1@live.com.ar>
2022-03-27 16:43:46 -0300

    correcion de nombre de atributo

--- rectangulo.py
+++ rectangulo.py
@@ -3,9 +3,9 @@ class Rectangulo:
         self.base = base
         self.altura = altura
 
-    def area(self):
+    def CalcularArea(self):
         return f"Area del rectangulo: {self.base * self.altura}"
 
 
 p = Rectangulo(int(input("Proporciona la base: ")), int(input("Proporciona la altura: ")))
-print(p.area())
+print(p.CalcularArea())
