4191710bf32e5e054e5de0a6a60ae5c36c63a9c5
Lucas Araujo <lsa1@live.com.ar>
2022-03-29 10:06:53 -0300

    encapsulamiento metodo de señalizar

---
+++ encapsulamiento.py
@@ -0,0 +1,12 @@
+class Persona:
+    def __init__(self, nombre, apellido, edad):
+        self._nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def Mostrar_detalle(self):
+        print(f"persona: {self._nombre}, {self.apellido} {self.edad}")
+
+persona1 = Persona("Juan", "Perez", 28)
+persona1._nombre = "Juan Carlos"
+persona1.Mostrar_detalle()
