d7c280548b042ee1dbeaf468193df08190646957
Lucas Araujo <lsa1@live.com.ar>
2022-04-02 10:59:54 -0300

    Sobre escritura del metodo __str__.

--- Herencia en python/Persona.py
+++ Herencia en python/Persona.py
@@ -3,12 +3,13 @@ class Persona:
         self.nombre = nombre
         self.edad = edad
 
+    def __str__(self):
+        return f"Persona: {self.nombre}, Edad: {self.edad}"
+
 class Empleado(Persona):
     def __init__(self, nombre, edad, sueldo):
         super().__init__(nombre, edad)
         self.sueldo = sueldo
 
-empleado1 = Empleado("juan",28,5000)
-print(empleado1.nombre)
-print(empleado1.edad)
-print(empleado1.sueldo)
+    def __str__(self):
+        return f"{super().__str__()} Sueldo: {self.sueldo}"
