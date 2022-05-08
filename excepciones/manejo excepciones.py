4a737cf1d009ed882bcc20e381026145a2e15ee7
Lucas Araujo <lsa1@live.com.ar>
2022-05-08 19:53:15 -0300

    Variables afuera y adentro del bloque de excepciones

--- excepciones/manejo excepciones.py
+++ excepciones/manejo excepciones.py
@@ -1,13 +1,15 @@
 resultado = None
-a = "10"
-b = 0
-
 try:
-   resultado = a/b
+    a = int(input("Primer numero: "))
+    b = int(input("segundo numero: "))
+    resultado = a/b
+
 except ZeroDivisionError as ze:
     print(f"Tenemos un error: {ze}")
 except TypeError as te:
     print(f"Tenemos un error: {te}")
+except Exception as e:
+    print(f"Tenemos un error: {e}")
 
 print(f"Resultado = {resultado}")
 print("cotinuamos...")
