fee560526ca566a1a2057953f620bfb975d61c87
Lucas Araujo <lsa1@live.com.ar>
2022-05-08 19:13:09 -0300

    Excepciones mas especificos

--- excepciones/manejo excepciones.py
+++ excepciones/manejo excepciones.py
@@ -1,11 +1,13 @@
 resultado = None
-a = 10
+a = "10"
 b = 0
 
 try:
    resultado = a/b
-except Exception as e:
-    print(f"Tenemos un error: {e}")
+except ZeroDivisionError as ze:
+    print(f"Tenemos un error: {ze}")
+except TypeError as te:
+    print(f"Tenemos un error: {te}")
 
 print(f"Resultado = {resultado}")
 print("cotinuamos...")
