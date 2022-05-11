5242c9a06ad9b26b2c08040c52018c171eda8a7b
Lucas Araujo <lsa1@live.com.ar>
2022-05-11 11:42:24 -0300

    codificaion de caracteres y indicacion de cierre de archivo

---
+++ manejo de archivos/manejo_archivos.py
@@ -0,0 +1,10 @@
+try:
+    archivo = open("prueba.txt", "w", encoding="utf8")
+    archivo.write("Agregamos info al archivo")
+    archivo.write("Adios")
+except Exception as e:
+    print(e)
+finally:
+    archivo.close()
+    print("fin del archivo") # ya no puede editar el archivo
+    # arcivo.write("nueva info)
