8e09786e821bfee7b5bdef12c5b47f7e02679987
Lucas Araujo <lsa1@live.com.ar>
2022-05-12 19:54:12 -0300

    mas formas de trabajar con archivos

--- manejo de archivos/Lee_Archivos.py
+++ manejo de archivos/Lee_Archivos.py
@@ -5,5 +5,23 @@ archivo = open("prueba.txt", "r", encoding="utf8")
 #print(archivo.read(5))
 
 #leer lineas completas
-print(archivo.readline())
-print(archivo.readline())
+#print(archivo.readline())
+#print(archivo.readline())
+
+#iterar el archivo
+#for linea in archivo:
+#    print(linea)
+
+# leer
+#print(archivo.readlines())
+
+# acceder a una lista de la linea
+#print(archivo.readlines()[0])
+
+# abrimos un archivo
+# a - anexar imformacion
+archivo2 = open("copia.txt", "a")
+archivo2.write(archivo.read())
+
+archivo.close()
+archivo2.close()
