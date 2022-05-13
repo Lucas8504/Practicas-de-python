3207bd22415ff5e1dad522f0d1d22d1a704219f8
Lucas Araujo <lsa1@live.com.ar>
2022-05-12 21:30:49 -0300

    with, context manager y mod de gitignore

--- manejo de archivos/Archivos_con_with.py
+++ manejo de archivos/Archivos_con_with.py
@@ -1,2 +1,5 @@
-with open("prueba.txt", "r", encoding="utf8") as archivo:
+from ManejoArchivos import ManejoDeArchivos
+#with open("prueba.txt", "r", encoding="utf8") as archivo:
+
+with ManejoDeArchivos("prueba.txt") as archivo:
     print(archivo.read())
