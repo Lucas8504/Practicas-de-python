3ef7624a2f992fe2aa3eebc31d92a871c95f038f
Lucas Araujo <lsa1@live.com.ar>
2022-05-15 19:27:06 -0300

    Vriable de clase al vuelo

---
+++ Contexto estatico (de clase)/MiClase.py
@@ -0,0 +1,18 @@
+class MiClase:
+
+    vaiables_clase = "valor variable clase"
+
+    def __init__(self, variable_instancia):
+        self.vaiables_instancia = variable_instancia
+
+print(MiClase.vaiables_clase)
+miClase = MiClase("valor variable instancia")
+print(miClase.vaiables_instancia)
+print(miClase.vaiables_clase)
+
+MiClase.vaiables_clase2 = "alor variable clase 2"
+
+miClase2 = MiClase("Otro valor de variable de instancia")
+print(miClase2.vaiables_instancia)
+print(miClase2.vaiables_clase)
+print(miClase.vaiables_clase2)
