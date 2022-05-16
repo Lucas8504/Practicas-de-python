8c1c64ecd1b7eefe92bc1cb078d3dcf4c45ae844
Lucas Araujo <lsa1@live.com.ar>
2022-05-16 20:00:28 -0300

    Constantes

---
+++ Contexto estatico (de clase)/test_constantes.py
@@ -0,0 +1,9 @@
+from Constantes import MI_CONSTANTE
+from Constantes import Matematicas as mate
+
+print(MI_CONSTANTE)
+# No devemos cambiar el valor de una constante
+# MI_CONSTANTE = "Nuevo valor"
+
+print(mate.PI)
+
