da9fae2c484651393a1be9c52d3f1de711a6013e
Lucas Araujo <lsa1@live.com.ar>
2022-10-12 19:45:24 -0300

    Introduccion a tkinter

---
+++ 01 Tkinter/TkHolamundo.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Creamos un objeto usando la clase Tk
+ventana = tk.Tk()
+# Establecemos un tamaño de la ventana en pixeles
+ventana.geometry('600x400')
+# Cambiamos el nombre de la ventana
+ventana.title('Hola Mundo')
+# Iniciamos la ventana (esta línea la ejecutamos al final)
+# Si la ejecutamos antes, no se muestran los cambios anteriores
+ventana.mainloop()
