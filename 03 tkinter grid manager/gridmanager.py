d8cef78712e0ecad07050d38be042f1a030d435f
Lucas Araujo <lsa1@live.com.ar>
2022-10-30 13:41:41 -0300

    comfiguracion de grid manager

--- 03 tkinter grid manager/gridmanager.py
+++ 03 tkinter grid manager/gridmanager.py
@@ -11,6 +11,12 @@ windows.title('Hola Mundo')
 # agregamos un icono
 windows.iconbitmap('icono.ico')
 
+# comfig Del gridmanager
+windows.rowconfigure(0, weight=1)
+windows.rowconfigure(1, weight=5)
+windows.columnconfigure(0, weight=1)
+windows.columnconfigure(1, weight=5)
+
 
 #Metodos de los eventos
 def evento1():
@@ -26,7 +32,15 @@ boton1.grid(row=0, column=0)
 # Definimos boton 2
 # *dsticky N(arriba), E(derecha), S(abajo), W(izquierda)
 boton2 = ttk.Button(windows, text='boton 2', command=evento2)
-boton2.grid(row=2, column=0, sticky='W')  # *
+boton2.grid(row=1, column=0, sticky='NSWE')  # *
+
+# Definimos boton 3
+boton3 = ttk.Button(windows, text="boton 3")
+boton3.grid(row=0, column=1, sticky="NSWE")
+
+# definimos boton 4
+boton4 = ttk.Button(windows, text="boton 4")
+boton4.grid(row=1, column=1)
 
 
 windows.mainloop()
